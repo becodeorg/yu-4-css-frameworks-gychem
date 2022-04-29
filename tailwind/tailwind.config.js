18d635f55bedcac44e0eb5d5795ef5b9b9f7110d
Gychem Baert <gychem.baert@hotmail.com>
2022-04-29 13:52:28 +0200

    Finished header styling

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -2,10 +2,7 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'manimage': "url('../dist/images/man.png')",
-        
-      }
+    
     },
 
   },
